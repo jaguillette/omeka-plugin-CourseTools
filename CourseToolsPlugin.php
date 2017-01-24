2573d8a781bd3ebc34c1ee25307b6cd35380b893
Jeremy Guillette <jguillette@fas.harvard.edu>
2017-01-24 16:55:30 -0500

    set up dynamic permissions through plugin config
    Dynamic permissions only apply to students, and use several arrays defined by the plugin

--- CourseToolsPlugin.php
+++ CourseToolsPlugin.php
@@ -11,6 +11,9 @@ class CourseToolsPlugin extends Omeka_Plugin_AbstractPlugin
 
   public function hookConfigForm($args)
   {
+    $active_content_types = $this->active_content_types;
+    $content_types = $this->content_types;
+    $permissions = $this->permissions;
     include "config_form.php";
   }
 
@@ -21,7 +24,10 @@ class CourseToolsPlugin extends Omeka_Plugin_AbstractPlugin
 
   public function hookAdminHead($args)
   {
-    queue_css_string($this->make_disabling_css());
+    $user = current_user();
+    if ($user && $user->getRoleId() == 'student') {
+      queue_css_string($this->make_disabling_css());
+    }
   }
 
   public function hookDefineAcl($args)
@@ -29,22 +35,71 @@ class CourseToolsPlugin extends Omeka_Plugin_AbstractPlugin
     $acl = $args['acl'];
 
     # Define available content_types
-    $content_types = array('Items','Collections');
+    $this->active_content_types = array('Items','Collections');
     if ($acl->has('ExhibitBuilder_Exhibits')) {
-      array_push($content_types, 'ExhibitBuilder_Exhibits');
+      array_push($this->active_content_types, 'ExhibitBuilder_Exhibits');
     }
     if ($acl->has('Neatline_Exhibits')) {
-      array_push($content_types, 'Neatline_Exhibits');
+      array_push($this->active_content_types, 'Neatline_Exhibits');
     }
 
-    # Add new role, student, which inherits from contributor, and allow the role
-    # to make own content public.
-    $acl->addRole('student','contributor');
-    $acl->allow('student', $content_types, 'makePublic', new Omeka_Acl_Assert_Ownership);
+    # Add role, no inherited permissions other than global.
+    $acl->addRole('student');
+
+    # Allow students to edit own files, autocomplete tags.
+    # These permissions don't fit into the defined permissions, but are needed.
+    $acl->allow('student','Files','editSelf');
+    $acl->allow('student','Tags',array('autocomplete'));
+
+    # Iterate through available content types
+    foreach ($this->active_content_types as $content_type) {
+      $all_permissions = array();
+      $own_permissions = array();
+      foreach ($this->permissions as $permission_key => $permission_value) {
+        $the_option = "$permission_key|$content_type";
+        switch (get_option($the_option)) {
+          case 'all':
+            if (array_key_exists($content_type,$permission_value['permissions'])) {
+              # If there are more specific permissions that need to be applied for this content type, apply them.
+              foreach ($permission_value['permissions'][$content_type]['all'] as $resource_permissions) {
+                if (array_key_exists('assertion',$resource_permissions)) {
+                  $acl->allow('student',$resource_permissions['resource'],$resource_permissions['permissions'],new $resource_permissions['assertion']);
+                } else {
+                  $acl->allow('student',$resource_permissions['resource'],$resource_permissions['permissions']);
+                }
+              }
+            } else {
+              # Otherwise apply simpler default permissions
+              $acl->allow('student',$content_type,$permission_value['permissions']['default']['all']);
+            }
+            break;
+          case 'own':
+            if (array_key_exists($content_type,$permission_value['permissions'])) {
+              # If there are more specific permissions that need to be applied for this content type, apply them.
+              foreach ($permission_value['permissions'][$content_type]['own'] as $resource_permissions) {
+                if (array_key_exists('assertion',$resource_permissions)) {
+                  $acl->allow('student',$resource_permissions['resource'],$resource_permissions['permissions'],new $resource_permissions['assertion']);
+                } else {
+                  $acl->allow('student',$resource_permissions['resource'],$resource_permissions['permissions']);
+                }
+              }
+            } else {
+              # Otherwise apply simpler default permissions
+              $acl->allow('student',$content_type,$permission_value['permissions']['default']['own']);
+            }
+            break;
+          default:
+            # code...
+            break;
+        }
+      }
+    }
 
     # Add a new role, reviewer, which has more permissions to view non-public content
     $acl->addRole('reviewer','researcher');
-    $acl->allow('reviewer', $content_types, 'showNotPublic');
+    foreach ($this->active_content_types as $active_type) {
+      $acl->allow('reviewer', $this->content_types[$active_type]['resources'], 'showNotPublic');
+    }
   }
 
   public function setOptions($options)
@@ -57,10 +112,10 @@ class CourseToolsPlugin extends Omeka_Plugin_AbstractPlugin
   public function make_disabling_css()
   {
     $disable_array = array(
-      "course_tool_disable_item_delete" => "body.items .delete-confirm { display:none; }",
-      "course_tool_disable_collection_delete" => "body.collections .delete-confirm { display:none; }",
-      "course_tool_disable_exhibit_delete" => "body.exhibits .delete-confirm { display:none; }",
-      "course_tool_disable_neatline_delete" => "table.neatline .delete-confirm { display:none; }"
+      "delete|Items" => "body.items .delete-confirm { display:none; }",
+      "delete|Collections" => "body.collections .delete-confirm { display:none; }",
+      "delete|ExhibitBuilder_Exhibits" => "body.exhibits .delete-confirm { display:none; }",
+      "delete|Neatline_Exhibits" => "table.neatline .delete-confirm { display:none; }"
     );
     $style = "";
     foreach ($disable_array as $key => $value) {
@@ -70,6 +125,140 @@ class CourseToolsPlugin extends Omeka_Plugin_AbstractPlugin
     }
     return $style;
   }
+
+  public $content_types = array(
+    'Items'=>array(
+      'resources'=>array('Items'),
+      'label'=>'Items'
+    ),
+    'Collections'=>array(
+      'resources'=>array('Collections'),
+      'label'=>'Collections'
+    ),
+    'ExhibitBuilder_Exhibits'=>array(
+      'resources'=>array('ExhibitBuilder_Exhibits'),
+      'label'=>'Exhibit Builder Exhibits'
+    ),
+    'Neatline_Exhibits'=>array(
+      'resources'=>array('Neatline_Exhibits','Neatline_Records'),
+      'label'=>'Neatline Exhibits'
+    )
+  );
+
+  public $active_content_types = array();
+
+  public $permissions = array(
+      'view-non-public'=>array(
+        'title'=>'View Non-Public...',
+        'description'=>"View the resource type, even if its creator hasn't made it publicly accessible.
+                        Anyone can always view content that has been made public.",
+        'permissions'=>array(
+          'default'=>array(
+            'all'=>array('showNotPublic'),
+            'own'=>array('showSelfNotPublic')
+          )
+        )
+      ),
+      'create-edit'=>array(
+        'title'=>'Create/Edit...',
+        'description'=>"Allow students to create and edit content of this resource type.
+                        Granting edit access also grants view access, overriding view permissions.
+                        Edit access also grants students the ability to make their work public.",
+        'permissions'=>array(
+          'Neatline_Exhibits'=>array(
+            'all'=>array(
+              array(
+                'resource'=>'Neatline_Exhibits',
+                'permissions'=>array('edit','editor','put','import','showNotPublic')
+              ),
+              array(
+                'resource'=>'Neatline_Records',
+                'permissions'=>array('post','put')
+              )
+            ),
+            'own'=>array(
+              array(
+                'resource'=>'Neatline_Exhibits',
+                'permissions'=>array('edit','editor','put','import','showNotPublic'),
+                'assertion'=>'Omeka_Acl_Assert_Ownership'
+              ),
+              array(
+                'resource'=>'Neatline_Records',
+                'permissions'=>array('post','put'),
+                // 'assertion'=>'Neatline_Acl_Assert_RecordOwnership'
+                // Assertion above seems to be part of Neatline's permissions, but bugs out when implemented
+              ),
+              array(
+                'resource'=>'Neatline_Exhibits',
+                'permissions'=>array('showSelfNotPublic','editSelf','editorSelf','putSelf','importSelf','deleteSelf')
+              ),
+              array(
+                'resource'=>'Neatline_Records',
+                'permissions'=>array('postSelf','putSelf')
+              )
+            )
+          ),
+          'default'=>array(
+            'all'=>array(
+              'add',
+              'tag',
+              'batch-edit',
+              'batch-edit-save',
+              'change-type',
+              'edit',
+              'showSelfNotPublic',
+              'showNotPublic',
+              'makePublic',
+              'add-page',               # From Exhibit builder
+              'edit-page',              # From Exhibit builder
+              'attachment',             # From Exhibit builder
+              'attachment-item-options',# From Exhibit builder
+              'theme-config',           # From Exhibit builder
+              'block-form',             # From Exhibit builder
+              'editor',                 # From Neatline
+              'put',                    # From Neatline
+              'import',                 # From Neatline
+              'post',                   # From Neatline
+            ),
+            'own'=>array(
+              'add',
+              'tag',
+              'batch-edit',
+              'batch-edit-save',
+              'change-type',
+              'editSelf',
+              'showSelfNotPublic',
+              'makePublic',
+              'add-page',               # From Exhibit builder
+              'edit-page',              # From Exhibit builder
+              'attachment',             # From Exhibit builder
+              'attachment-item-options',# From Exhibit builder
+              'theme-config',           # From Exhibit builder
+              'block-form',             # From Exhibit builder
+              'editorSelf',             # From Neatline
+              'putSelf',                # From Neatline
+              'importSelf',             # From Neatline
+              'post',                   # From Neatline
+            )
+          )
+        )
+      ),
+      'delete'=>array(
+        'title'=>"Delete...",
+        'description'=>"Allow users to delete content of this resource type.
+                        If students are given create/edit access to content,
+                        but not delete access, they can create that kind of
+                        content, but not delete it. This may be useful in
+                        preventing accidental deletion of projects close to
+                        deadlines.",
+        'permissions'=>array(
+          'default'=>array(
+            'all'=>array('delete','delete-confirm'),
+            'own'=>array('deleteSelf','delete-confirm')
+          )
+        )
+      )
+    );
 }
 
 ?>
--- config_form.php
+++ config_form.php
@@ -1,34 +1,38 @@
-<h2><?php echo __("Disable deletion of content by type"); ?></h2>
-<p class="explanation"><?php echo __("For each type of content, checking the box will hide the interface elements for deleting that kind of content. It is still technically possible for a user to delete that content, but it is not possible to do it accidentally.") ?></p>
-<div class="field">
-  <div class="two columns alpha">
-    <label for="course_tool_disable_item_delete"><?php echo __("Disable Item Deletion"); ?></label>
-  </div>
-  <div class="inputs five columns omega">
-    <?php echo get_view()->formCheckbox('course_tool_disable_item_delete',null,array('checked' => get_option('course_tool_disable_item_delete'))); ?>
-  </div>
-</div>
-<div class="field">
-  <div class="two columns alpha">
-    <label for="course_tool_disable_collection_delete"><?php echo __("Disable Collection Deletion"); ?></label>
-  </div>
-  <div class="inputs five columns omega">
-    <?php echo get_view()->formCheckbox('course_tool_disable_collection_delete',null,array('checked' => get_option('course_tool_disable_collection_delete'))); ?>
-  </div>
-</div>
-<div class="field">
-  <div class="two columns alpha">
-    <label for="course_tool_disable_exhibit_delete"><?php echo __("Disable Exhibit Deletion"); ?></label>
-  </div>
-  <div class="inputs five columns omega">
-    <?php echo get_view()->formCheckbox('course_tool_disable_exhibit_delete',null,array('checked' => get_option('course_tool_disable_exhibit_delete'))); ?>
-  </div>
-</div>
-<div class="field">
-  <div class="two columns alpha">
-    <label for="course_tool_disable_neatline_delete"><?php echo __("Disable Neatline Exhibit Deletion"); ?></label>
-  </div>
-  <div class="inputs five columns omega">
-    <?php echo get_view()->formCheckbox('course_tool_disable_neatline_delete',null,array('checked' => get_option('course_tool_disable_neatline_delete'))); ?>
-  </div>
+<style>
+.content-type.permission {display: inline-block;}
+.debug {position: absolute; top:45px; left: 20px; z-index: 10; background-color: white;}
+.permission input {margin: 0 45%;}
+.permission label {width: 33%; display: inline-block;}
+.permission label.row-label {width: 100%;}
+.permission-label {text-align: center; width: 32.5%; display: inline-block;}
+.permission-labels {float: right;}
+</style>
+<div class="set-permissions" style="display:inline-block;">
+  <h2><?php echo __("Set Permissions for Students"); ?></h2>
+  <p class="explanation"><?php echo __("Select what permissions students should have for each type of content."); ?></p>
+  <?php
+  foreach ($permissions as $key => $value): ?>
+    <h3><?php echo $value['title']; ?></h3>
+    <p class="explanation"><?php echo $value['description']; ?></p>
+    <div class="four columns alpha omega permission-labels">
+      <div class="permission-label">All</div>
+      <div class="permission-label">Own</div>
+      <div class="permission-label">None</div>
+    </div>
+    <?php foreach ($active_content_types as $type):
+      $keytype = "$key|$type";
+      $label = $content_types[$type]['label']; ?>
+      <div class="content-type permission">
+        <div class="three columns alpha">
+          <label class="row-label" for="<?php echo $keytype; ?>"><?php echo $label; ?></label>
+        </div>
+        <div class="four columns omega">
+          <?php
+            // echo get_view()->formRadio($id, $value, $attribs, $options, $listsep);
+            echo get_view()->formRadio($keytype, get_option($keytype), array('class'=>'columns omega'), array('all'=>'','own'=>'','none'=>''),"");
+          ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  <?php endforeach; ?>
 </div>
